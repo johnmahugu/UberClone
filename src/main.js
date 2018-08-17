1097e6c1662cc2ddd7a9075d3bb07903c0e8ff23
SeanKim <sean.kim84@gmail.com>
2018-08-17 10:44:04 +0700

    App Configuraition

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet ,View, Text} from 'react-native';
+
+
+class Main extends Component{
+    render(){
+        return(
+            <View>
+                <Text>I like Grab rather than Uber</Text>
+            </View>
+        )
+    }
+}
+
+export default Main;
